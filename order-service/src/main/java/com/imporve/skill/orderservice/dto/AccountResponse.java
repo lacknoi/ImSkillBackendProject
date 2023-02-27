c8f1e0aed156c302993e3eefc1eb2bd4d941143b
CORP-AIS900\noppholn <noppholn@ais.co.th>
2023-02-27 20:00:09 +0700

    order call internal account-service

---
+++ order-service/src/main/java/com/imporve/skill/orderservice/dto/AccountResponse.java
@@ -0,0 +1,15 @@
+package com.imporve.skill.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountResponse {
+	private String accntNo;
+	private String accntName;
+}
--- order-service/src/main/java/com/imporve/skill/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/imporve/skill/orderservice/service/OrderService.java
@@ -1,8 +1,14 @@
 package com.imporve.skill.orderservice.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.imporve.skill.orderservice.dto.AccountResponse;
 import com.imporve.skill.orderservice.dto.OrderRequest;
 import com.imporve.skill.orderservice.model.Order;
 import com.imporve.skill.orderservice.repository.OrderRepository;
@@ -13,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
-//	private final WebClient webClient;
+	private final WebClient webClient;
 	private final OrderRepository orderRepository;
 	
 	public void submitOrder(OrderRequest orderRequest) {
@@ -21,12 +27,21 @@ public class OrderService {
 		
 		Order configFile = orderRepository.getById(1L);
 		
-//		String s = webClient.get()
-//				.uri("http://localhost:8080/api/account/accounts")
-//				.retrieve()
-//				.bodyToMono(String.class)
-//				.block();
-//		
-		System.out.println(configFile.getParamType());
+		List<String> baNos = new ArrayList<>();
+		baNos.add("30900000000168");
+		baNos.add("31300000806387");
+		
+		AccountResponse[] accountArr = webClient.get()
+				.uri("http://localhost:8080/api/account",
+						uriBuilder -> uriBuilder.queryParam("baNo", baNos).build())
+				.retrieve()
+				.bodyToMono(AccountResponse[].class)
+				.block();
+		
+		List<AccountResponse> accounts = Arrays.asList(accountArr);
+		
+		for(AccountResponse accountResponse : accounts) {
+			System.out.println(accountResponse.getAccntNo() + ":" + accountResponse.getAccntName());
+		}
 	}
 }
